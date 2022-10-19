38d0b7103d27c60494143846664242a1daa17f8b
Ho Viet Dung <vietdung040490@gmail.com>
2022-10-19 13:43:54 +0900

    update

--- 電力グラフ/index.js
+++ 電力グラフ/index.js
@@ -1,64 +1,67 @@
+let myChart = document.getElementById('myChart').getContext('2d');
+// Global Options
+Chart.defaults.global.defaultFontFamily = 'Lato';
+Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = '#777';
 
-    let myChart = document.getElementById('myChart').getContext('2d');
-    // Global Options
-    Chart.defaults.global.defaultFontFamily = 'Lato';
-    Chart.defaults.global.defaultFontSize = 18;
-    Chart.defaults.global.defaultFontColor = '#777';
-
-    let massPopChart = new Chart(myChart, {
-      type:'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-      data:{
-        labels:['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
-        datasets:[{
-          label:'Population',
-          data:[
-            617594,
-            181045,
-            153060,
-            106519,
-            105162,
-            95072
-          ],
-          //backgroundColor:'green',
-          backgroundColor:[
-            'red',
-            'red)',
-            'red)',
-            'red)',
-            'red)',
-            'red)',
-            'red)'
-          ],
-          borderWidth:1,
-          borderColor:'#777',
-          hoverBorderWidth:3,
-          hoverBorderColor:'#000'
+let myFristChart = new Chart(myChart, {
+    type: 'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+    data: {
+        labels: ['電力','電力','電力','電力','電力','電力','電力','電力','電力','電力',
+        '電力','電力','電力','電力','電力','電力','電力','電力','電力','電力','電力','電力','電力',],
+        datasets: [{
+            label: '電力グラフ',
+            data: [
+                1, 2, 3, 4, 5, 4,
+                1, 2, 3, 4, 5, 4,
+                1, 2, 3, 4, 5, 4,
+                1, 2, 3, 4, 5, 4,
+            ],
+            backgroundColor: [
+                'red', 'red', 'red', 'red', 'red', 'red',
+                'red', 'red', 'red', 'red', 'red', 'red',
+                'red', 'red', 'red', 'red', 'red', 'red',
+                'red', 'red', 'red', 'red', 'red', 'red',
+            ],
+            borderWidth: 1,
+            borderColor: '#777',
+            hoverBorderWidth: 3,
+            hoverBorderColor: '#000'
         }]
-      },
-      options:{
-        title:{
-          display:true,
-          text:'Largest Cities In Massachusetts',
-          fontSize:25
+    },
+    options: {
+        title: {
+            display: true,
+            text: '電力グラフ',
+            fontSize: 55
         },
-        legend:{
-          display:true,
-          position:'right',
-          labels:{
-            fontColor:'#000'
-          }
+        legend: {
+            display: true,
+            position: 'right',
+            labels: {
+                fontColor: '#000'
+            }
         },
-        layout:{
-          padding:{
-            left:50,
-            right:0,
-            bottom:0,
-            top:0
-          }
+        layout: {
+            padding: {
+                left: 50,
+                right: 0,
+                bottom: 0,
+                top: 0
+            }
         },
-        tooltips:{
-          enabled:true
+        tooltips: {
+            enabled: true
         }
-      }
-    });
+    }
+});
 
+
+setInterval(function() {
+    let data = []
+    for (let i = 0; i < 23; i++) {
+        data[i] = Math.floor(Math.random() * 8000);
+    }
+    myFristChart.data.datasets[0].data = data
+    myFristChart.update();
+}, 1000);
