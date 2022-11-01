38bb35fbf3efc82d7d676f59b28fbfda08a7e6d5
Ho Viet Dung <vietdung040490@gmail.com>
2022-11-01 21:02:14 +0900

    update

---
+++ 履歴/rireki.js
@@ -0,0 +1,616 @@
+let eventsArray = [
+    {
+      timeCount: 1,
+      time: '2022 November 1st, 8:33:04 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 2,
+      time: '2022 November 1st, 8:33:05 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 3,
+      time: '2022 November 1st, 8:33:06 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 4,
+      time: '2022 November 1st, 8:33:07 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 5,
+      time: '2022 November 1st, 8:33:08 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 6,
+      time: '2022 November 1st, 8:33:09 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 7,
+      time: '2022 November 1st, 8:33:10 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 8,
+      time: '2022 November 1st, 8:33:11 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 9,
+      time: '2022 November 1st, 8:33:12 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 10,
+      time: '2022 November 1st, 8:33:13 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 11,
+      time: '2022 November 1st, 8:33:14 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 12,
+      time: '2022 November 1st, 8:33:15 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 13,
+      time: '2022 November 1st, 8:33:16 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 14,
+      time: '2022 November 1st, 8:33:17 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 15,
+      time: '2022 November 1st, 8:33:18 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 16,
+      time: '2022 November 1st, 8:33:19 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 17,
+      time: '2022 November 1st, 8:33:20 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 18,
+      time: '2022 November 1st, 8:33:21 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 19,
+      time: '2022 November 1st, 8:33:22 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 20,
+      time: '2022 November 1st, 8:33:23 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 21,
+      time: '2022 November 1st, 8:33:24 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 22,
+      time: '2022 November 1st, 8:33:25 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 23,
+      time: '2022 November 1st, 8:33:26 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 24,
+      time: '2022 November 1st, 8:33:27 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 25,
+      time: '2022 November 1st, 8:33:28 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 26,
+      time: '2022 November 1st, 8:33:29 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 27,
+      time: '2022 November 1st, 8:33:30 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 28,
+      time: '2022 November 1st, 8:33:31 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 29,
+      time: '2022 November 1st, 8:33:32 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 30,
+      time: '2022 November 1st, 8:33:33 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 31,
+      time: '2022 November 1st, 8:33:34 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 32,
+      time: '2022 November 1st, 8:33:35 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 33,
+      time: '2022 November 1st, 8:33:36 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 34,
+      time: '2022 November 1st, 8:33:37 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 35,
+      time: '2022 November 1st, 8:33:38 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 36,
+      time: '2022 November 1st, 8:33:39 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 37,
+      time: '2022 November 1st, 8:33:40 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 38,
+      time: '2022 November 1st, 8:33:41 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 39,
+      time: '2022 November 1st, 8:33:42 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 40,
+      time: '2022 November 1st, 8:33:43 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 41,
+      time: '2022 November 1st, 8:33:44 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 42,
+      time: '2022 November 1st, 8:33:45 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 43,
+      time: '2022 November 1st, 8:33:46 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 44,
+      time: '2022 November 1st, 8:33:47 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 45,
+      time: '2022 November 1st, 8:33:48 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 46,
+      time: '2022 November 1st, 8:33:49 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 47,
+      time: '2022 November 1st, 8:33:50 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 48,
+      time: '2022 November 1st, 8:33:51 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 49,
+      time: '2022 November 1st, 8:33:52 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 50,
+      time: '2022 November 1st, 8:33:53 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 51,
+      time: '2022 November 1st, 8:33:54 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 52,
+      time: '2022 November 1st, 8:33:55 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 53,
+      time: '2022 November 1st, 8:33:56 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 54,
+      time: '2022 November 1st, 8:33:57 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 55,
+      time: '2022 November 1st, 8:33:58 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 56,
+      time: '2022 November 1st, 8:33:59 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 57,
+      time: '2022 November 1st, 8:34:00 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 58,
+      time: '2022 November 1st, 8:34:01 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 59,
+      time: '2022 November 1st, 8:34:02 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 60,
+      time: '2022 November 1st, 8:34:03 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 61,
+      time: '2022 November 1st, 8:34:04 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 62,
+      time: '2022 November 1st, 8:34:05 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 63,
+      time: '2022 November 1st, 8:34:06 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 64,
+      time: '2022 November 1st, 8:34:07 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 65,
+      time: '2022 November 1st, 8:34:08 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 66,
+      time: '2022 November 1st, 8:34:09 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 67,
+      time: '2022 November 1st, 8:34:10 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 68,
+      time: '2022 November 1st, 8:34:11 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 69,
+      time: '2022 November 1st, 8:34:12 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 70,
+      time: '2022 November 1st, 8:34:13 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 71,
+      time: '2022 November 1st, 8:34:14 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 72,
+      time: '2022 November 1st, 8:34:15 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 73,
+      time: '2022 November 1st, 8:34:16 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 74,
+      time: '2022 November 1st, 8:34:17 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 75,
+      time: '2022 November 1st, 8:34:18 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    },
+    {
+      timeCount: 76,
+      time: '2022 November 1st, 8:34:19 pm',
+      kind: '種別',
+      device: 'PC',
+      content: 'ログイン'
+    }
+  ];
+
+let currentEventsPerPage = [];
+let currentPage = 1;
+let perPage = 17;
+let totalPages = Math.ceil(eventsArray.length / perPage);
+
+
+renderEvents = (currentPage, array) => {
+    currentEventsPerPage = array.slice(
+        (currentPage - 1) * perPage,
+        (currentPage - 1) * perPage + perPage);
+
+    // let events = currentEventsPerPage.map((event) => `<tr>
+    //                 <td>${event.time}</td>
+    //                 <td>${event.kind}</td>
+    //                 <td>${event.device}</td>
+    //                 <td>${event.content}</td>
+    //                 </tr>`
+    // ).join('');
+
+    let events = '';
+    // let count = 0;
+    for (let i = 0; i < currentEventsPerPage.length; i++) {
+        // count = Math.ceil(Math.random() * 10);
+        events += `<tr>
+                       <td>${currentEventsPerPage[i].time}</td>
+                       <td>${currentEventsPerPage[i].kind}</td>
+                       <td>${currentEventsPerPage[i].device}</td>
+                        <td>${currentEventsPerPage[i].content}</td>
+                        </tr>`;
+    }
+
+    document.getElementById('getData').innerHTML = events;
+    document.getElementById('currentPageNumber').innerHTML = `${currentPage}/${totalPages}`
+}
+
+// previos page
+handlePreviosPage = () => {
+    currentPage = currentPage - 1;
+    if (currentPage > 0) {
+        renderEvents(currentPage, eventsArray);
+    } else if (currentPage == 0) {
+        currentPage = currentPage + 1;
+        renderEvents(currentPage, eventsArray);
+    }
+}
+
+// nextpage
+handleNextPage = () => {
+    currentPage = currentPage + 1;
+    if (currentPage > 0 && currentPage <= totalPages) {
+        renderEvents(currentPage, eventsArray);
+    } else if (currentPage > totalPages) {
+        currentPage = currentPage - 1;
+        renderEvents(currentPage, eventsArray);
+    }
+}
+
+// 
+handleFirstPage = () => {
+    currentPage = 1;
+    renderEvents(currentPage, eventsArray);
+}
+
+handleLastPage = () => {
+    currentPage = totalPages;
+    renderEvents(currentPage, eventsArray);
+}
+
+// sắp xếp page mới đến cũ
+handleLatestEvents = () => {
+    let latestEvents = eventsArray.sort((a, b) => a.timeCount - b.timeCount)
+    renderEvents(currentPage, latestEvents);
+}
+
+//sắp xếp cũ đến mới
+handleOldestEvents = () => {
+    let oldestEvents = eventsArray.sort((a, b) => b.timeCount - a.timeCount)
+    renderEvents(currentPage, oldestEvents);
+}
+
+renderEvents(currentPage, eventsArray);
